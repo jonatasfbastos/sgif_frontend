ca09c97811f4e10d79f7196c05c9b96f3595ed4f
Kaio Fernandes <kaiofs122@gmail.com>
2023-05-20 22:03:58 -0300

    tela de importar professor totalmente fucnional

---
+++ br.com.ifba.importarprofessor/importarprofessor.js
@@ -0,0 +1,67 @@
+//função para selecionar o arquivo e enviar para ler
+function submit(){
+    var files = document.querySelector('#file').files;
+
+    if(files.length > 0 ){
+         var file = files[0];
+         readCSVFile(file);
+    }else{
+         alert("Por favor, selecione o arquivo.");
+    }
+
+}
+
+//função que lê o arquivo
+function readCSVFile(file){
+
+    var reader = new FileReader();
+
+    reader.readAsText(file);
+
+    reader.onload = function(event) {
+         var csvdata = event.target.result;
+         var rowData = csvdata.split('\n');
+         var header = rowData[0].replace('\r', "").split(",");
+
+         for (var row = 1; row < rowData.length; row++) {
+
+              var obj = {}
+
+              var linhaData = rowData[row].replace('\r', '').split(",")
+
+              //pegando dado por dado posição por posição e atribuindo a seu respectivo atributo para enviar
+              obj.nome = linhaData[0];
+              obj.telefone = linhaData[1];
+              obj.email = linhaData[2];
+              obj.cpf = linhaData[3];
+              obj.dataDeNascimento = new Date(linhaData[4].replace("/", "-"));
+              obj.siape = linhaData[5];
+
+               //está comentado para futuros testes caso necessário
+               /* for(var j = 0; j < header.length; j++){
+                   obj[header[j].replace(" ", "")] = linhaData[j];
+                   console.log("Header Row", header[j]);
+              }  */
+
+              //salvando o objeto caso o nome e o cpf sejam diferentes de vazio
+               if(obj.nome != "" && obj.cpf != ""){
+                    post('salvarProfessor', obj).then(result=>{
+                    }).catch(error=>{
+                         console.log('error', error)
+                    })
+            }  
+            
+         }
+
+    };
+
+}
+
+function importarPopupExibir(){
+     document.getElementById("popupImportar").classList.add("exibirImportar");
+ }
+
+ function importarPopupOcultar(){
+     document.getElementById("popupImportar").classList.remove("exibirImportar");
+ }
+ 
