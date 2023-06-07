a100871f362612d09eeee9c773ec5132f176e6ec
Gustavo Dourado Batista <gusd.batista@gmail.com>
2023-06-07 17:05:29 -0300

    ajustes na busca e edição das avaliações

--- br.com.ifba.avaliacao/avaliacao.js
+++ br.com.ifba.avaliacao/avaliacao.js
@@ -240,7 +240,7 @@ function buscar() {
     tr = table.getElementsByTagName("tr");
 
     for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[1];
+        td = tr[i].getElementsByTagName("td")[0];
         if (td) {
             txtValue = td.textContent || td.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
