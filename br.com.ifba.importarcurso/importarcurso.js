b98dba5af504e25789f15cad58fc7cec2adedf6d
PedroHSilva10 <20211ireads0014@ifba.edu.br>
2023-06-20 14:12:55 -0300

    atualizando importar curso

--- br.com.ifba.importarcurso/importarcurso.js
+++ br.com.ifba.importarcurso/importarcurso.js
@@ -3,15 +3,31 @@ const modal = document.querySelector("dialog")
 //função para selecionar o arquivo e enviar para ler
 function submit(){
      var files = document.querySelector('#file').files;
-
+ 
      if(files.length > 0 ){
           var file = files[0];
-          readCSVFile(file);
+          let verificaArquivo = file.name.slice(file.name.lastIndexOf(".") + 1)
+ 
+          var combobox = document.getElementById("comboBox");
+          var opcaoSelecionada = combobox.value;
+          
+          if(opcaoSelecionada == "valor1" && verificaArquivo == "csv"){
+               readCSVFile(file)
+          }else{
+               if(opcaoSelecionada == "valor2" && verificaArquivo == "xls"){
+                    handleFile(file)
+               }else{
+                    popupErroExibir("O arquivo selecionado não corresponde ao tipo escolhido!")
+                    importarPopupOcultar();
+               }
+          }
+ 
      }else{
-          alert("Por favor, selecione o arquivo.");
+          popupArquivoVazioExibir();
+          importarPopupOcultar();
      }
-
-}
+ 
+ }
 
 //função que lê o arquivo
 function readCSVFile(file){
@@ -48,8 +64,8 @@ function readCSVFile(file){
                    console.log("Header Row", header[j]);
                }  */
 
-              //salvando o objeto caso o nome e o cpf sejam diferentes de vazio
-               if(obj.nome != "" && obj.cpf != ""){
+              //salvando o objeto caso o nome e o codigoCurso sejam diferentes de vazio
+               if(obj.nome != "" && obj.codigoCurso != ""){
                     
                     post('salvarCurso', obj).then(result=>{
                          importarPopupExibir()
@@ -63,6 +79,59 @@ function readCSVFile(file){
      }
 
 }
+//função que lê o arquivo .XLS
+function handleFile(file) {
+
+     const reader = new FileReader();
+ 
+     reader.onload = function(e){
+          const data = new Uint8Array(e.target.result);
+          const workbook = XLSX.read(data, { type: 'array' });
+ 
+          // Seleciona a primeira planilha
+          const sheetName = workbook.SheetNames[0];
+          const worksheet = workbook.Sheets[sheetName];
+     
+          // Converte a planilha em um objeto JSON
+          const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+ 
+ 
+          let resultado = jsonData.filter(data => data.length > 0)
+ 
+          let flag = 0;
+ 
+          for (var row = 1; row < resultado.length; row++) {
+ 
+               var obj = {}
+ 
+               //pegando dado por dado posição por posição e atribuindo a seu respectivo atributo para enviar
+               obj.id = linhaData[0];
+               obj.nome = linhaData[1];
+               obj.codigoCurso = linhaData[2]
+               obj.sigla = linhaData[3];
+               obj.descricao = linhaData[4];
+               if(linhaData[5] == "True","TRUE","true","S","SIM","sim","Sim","ativa","Ativa","ATIVA" ){
+                    obj.ativo = "TRUE";
+               }else{
+                    obj.ativo = "FALSE";
+               }
+ 
+               //salvando o objeto caso o nome e o codigoCurso sejam diferentes de vazio
+               if(obj.nome != "" && obj.codigoCurso != ""){
+                    
+                    post('salvarCurso', obj).then(result=>{
+                         importarPopupExibir()
+                    }).catch(error=>{
+                         console.log('error', error)
+                    })
+               }       
+          } 
+          EnviarPopupExibir();
+          importarPopupOcultar();
+     };
+ 
+     reader.readAsArrayBuffer(file);
+ }
 
 function importarPopupExibir(){
      document.getElementById("popupConfirmar").classList.add("exibirImportar");
@@ -71,6 +140,40 @@ function importarPopupExibir(){
  function importarPopupOcultar(){
      document.getElementById("popupConfirmar").classList.remove("exibirImportar");
 }
+
+//Funções dos popups para a exibição e o ocultamento
+function importarPopupExibir(){
+     document.getElementById("popupImportar").classList.add("exibirImportar");
+  }
+ 
+ function importarPopupOcultar(){
+     document.getElementById("popupImportar").classList.remove("exibirImportar");
+  }
+ 
+ function popupArquivoVazioExibir(){
+     document.getElementById("popupArquivoVazio").classList.add("exibirVazio");
+  }
+ 
+ function popupArquivoVazioOcultar(){
+     document.getElementById("popupArquivoVazio").classList.remove("exibirVazio");
+  }
+ 
+ function popupErroExibir(mensagem){
+     document.getElementById("popupErro").classList.add("exibirErro");
+     document.getElementById("erro").innerText = mensagem;
+  }
+ 
+ function popupErroOcultar(){
+     document.getElementById("popupErro").classList.remove("exibirErro");
+  }
+ 
+ function EnviarPopupExibir(){
+     document.getElementById("popupEnviar").classList.add("exibirEnviar");
+ }
+ 
+ function EnviarPopupOcultar(){
+     document.getElementById("popupEnviar").classList.remove("exibirEnviar");
+ }
  
 
  
