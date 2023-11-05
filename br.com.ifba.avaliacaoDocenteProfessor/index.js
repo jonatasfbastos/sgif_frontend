d7d38ffadc2f94b516a75bd30e05d26eca5222b8
Eduardo Souza <103686965+DevEduardoSouza@users.noreply.github.com>
2023-11-05 06:56:43 -0300

    feat: Adiciona método para pegar todos os professores

--- br.com.ifba.avaliacaoDocenteProfessor/index.js
+++ br.com.ifba.avaliacaoDocenteProfessor/index.js
@@ -1,4 +1,31 @@
-const assessmentsDetails = document.querySelectorAll(".box-assessments");
+/**
+ * Um objeto utilitário para manipulação de elementos HTML.
+ * @namespace
+ * @description Este objeto fornece métodos para interagir com elementos HTML de forma simplificada.
+ */
+const html = {
+  get(selector) {
+    return document.querySelector(selector);
+  },
+  getAll(selector) {
+    return document.querySelectorAll(selector);
+  },
+  create(element) {
+    return document.createElement(element);
+  },
+  createElementWithClasses(element, ...classes) {
+    if (!element) return null;
+
+    const elementWithClasses = document.createElement(element);
+    if (classes.length > 0) {
+      elementWithClasses.classList.add(...classes);
+    }
+
+    return elementWithClasses;
+  },
+};
+
+const assessmentsDetails = html.getAll(".box-assessments");
 
 assessmentsDetails.forEach((assessment) => {
   assessment.addEventListener("click", () => {
@@ -6,3 +33,23 @@ assessmentsDetails.forEach((assessment) => {
     description.classList.toggle("active");
   });
 });
+
+/**---------{}----------**/
+const endpoint = "professor";
+
+const getAllTeachers = () => {
+  get(endpoint)
+    .then((data) => {
+      console.log("Data ", data);
+      //renderizar todos os professores
+      return data;
+    })
+    .catch((error) => {
+      console.log("Error ", error);
+    });
+};
+
+function init() {
+  getAllTeachers();
+}
+init();
