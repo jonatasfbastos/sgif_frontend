7e4f7aa1ac6b290ad0e5a471a7ade2eda374f156
Eduardo Souza <103686965+DevEduardoSouza@users.noreply.github.com>
2023-11-14 16:21:17 -0300

    feat: Adiciona funcionalidade inicial de controle de acesso

---
+++ js/accessControl.js
@@ -0,0 +1,21 @@
+// Suponha que você tenha obtido as permissões do usuário durante o login.
+var userPermissions = ['read_user']; // Pode conter várias permissões.
+
+
+// Função para verificar se o usuário tem uma permissão específica.
+function hasPermission(permission) {
+    return userPermissions.includes(permission);
+}
+
+// Verificar permissões e mostrar seções apropriadas.
+if (hasPermission('admin_access')) {
+    // Mostrar a seção de administrador.
+    console.log("o usuario é admin")
+    // document.getElementById('admin-section').style.display = 'block';
+  }
+  
+  if (hasPermission('read_user')) {
+  console.log("o usuario é user")
+    // Mostrar a seção de usuário.
+    // document.getElementById('user-section').style.display = 'block';
+}
